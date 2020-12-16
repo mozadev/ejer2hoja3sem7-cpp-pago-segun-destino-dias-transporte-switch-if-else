3f76448db11ec28822a31caefc9733e3e5157d43
CesarAroon <30559667+CesarAroon@users.noreply.github.com>
2020-12-16 01:22:24 -0500

    Add files via upload

---
+++ ejer2hoja3sema7carlosmere.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+using namespace System;
+
+int main()
+{
+	char Destino;
+	char Transporte;
+	int Dias;
+	float aux;
+	float Monto;
+	do{
+		do{
+			cout << "Ingrese Destino (C: Cajamarca; T: Tacna; H: Huaraz; F: Fin): ";
+			cin >> Destino;
+		} while (Destino != 'C' && Destino != 'T' && Destino != 'H' && Destino != 'F');
+		
+		do{
+			cout << "Ingrese Transporte (B: Bus; A: Avión): ";
+			cin >> Transporte;
+		} while (Transporte != 'B' && Transporte != 'A');
+
+		cout << "Ingrese Dias: ";
+		cin >> Dias;
+
+		if (Destino != 'F')
+		{
+			switch (Destino)
+			{
+			case 'C':
+				if (Transporte == 'B')
+					aux = 165;
+				else
+					aux = 84 * 2.68;
+				Monto = aux + Dias * 20 * 2.68;
+				break;
+			case 'T':
+				if (Transporte == 'B')
+					aux = 138;
+				else
+					aux = 133 * 2.68;
+				Monto = aux + Dias * 30 * 2.68;
+				break;
+			case 'H':
+				if (Transporte == 'B')
+					aux = 63;
+				Monto = aux + Dias * 30 * 2.68;
+				break;
+			}
+			
+			cout << "Monto a Pagar: " << Monto<<endl;
+		}
+	} while (Destino != 'F');
+
+	getch();
+	return 0;
+}
